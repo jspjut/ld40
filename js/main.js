20f488b56fb05b0c8a450fcfd55a733b8f76fcbd
Josef Spjut <josef.spjut@gmail.com>
2017-12-02 23:51:41 -0500

    Swirling stars

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ var mainState = {
     preload: function() {
         // initialization stuff before starting the game
         // asset loading here
-        // game.load.image('name', 'path/to/image.png');
+        game.load.image('star3', 'assets/star3.png');
     },
 
     create: function() {
@@ -17,13 +17,25 @@ var mainState = {
         game.world.enableBody = true;
 
         // create player, etc.
-        text = game.add.text(0, 0, "Ludum Dare 40!", {font: "62pt Arial Black", fill: "#c51b7d" });
+        text = game.add.text(0, 0, "Ludum Dare 40!", {font: "62pt Arial Black", fill: "#555b5d" });
         text.position.x = (game.world.width - text.width)/2;
         text.stroke = "#de77ae";
+
+        // add star
+        stars = game.add.group();
+        for (var i = 1; i < 7; i++) {
+            for (var j = 1; j < 5; j++) {
+                stars.add(game.add.sprite(200*i, 200*j, 'star3'));
+            }         
+        }
     },
 
     update: function() {
         // called once per frame at 60 Hz
+        for(var i = 0; i < stars.children.length; i++) {
+            stars.children[i].angle += 5;
+        }
+        // stars.angle += 5;
     }
 }
 
